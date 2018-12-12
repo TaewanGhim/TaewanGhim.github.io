e8e6215b2182b9d61da6e2e001188ba9fee0da39
TaewanGhim <wany@kaist.ac.kr>
2018-12-13 07:11:29 +0900

    Update hw4.js

--- asset/hw4/hw4.js
+++ asset/hw4/hw4.js
@@ -1,186 +1,5 @@
 
 
-'use strict';
-
-var shapes = [];
-
-var newShape;
-
-var joints = 12;
-var linelength = 64;
-var resolution = 0.06;
-var gravity = 0.094;
-var damping = 0.998;
-
-var showPath = true;
-var showPendulum = true;
-var showPendulumPath = true;
-var clearScreen = false;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 100);
-  noFill();
-  strokeWeight(1);
-}
-
-function draw() {
-  if (clearScreen) background(0, 0, 100);
-
-  shapes.forEach(function(shape) {
-    shape.draw();
-    shape.update();
-  });
-
-  if (newShape) {
-    newShape.addPos(mouseX, mouseY);
-    newShape.draw();
-    newShape.update();
-  }
-}
-
-function Shape(pendulumPathColor) {
-  this.shapePath = [];
-  this.pendulumPath = [];
-  this.pendulumPathColor = pendulumPathColor;
-  this.iterator = 0;
-  this.linelength = linelength;
-  this.resolution = resolution;
-  this.pendulum = new Pendulum(this.linelength, joints);
-
-  Shape.prototype.addPos = function(x, y) {
-    var newPos = createVector(x, y);
-    this.shapePath.push(newPos);
-  };
-
-  Shape.prototype.draw = function() {
-    strokeWeight(0.8);
-    stroke(0, 10);
-
-    if (showPath) {
-      beginShape();
-      this.shapePath.forEach(function(pos) {
-        vertex(pos.x, pos.y);
-      });
-      endShape();
-    }
-
-    if (this.iterator < this.shapePath.length) {
-      var currentIndex = floor(this.iterator);
-
-      var currentPos = this.shapePath[currentIndex];
-      var previousPos = this.shapePath[currentIndex - 1];
-      if (previousPos) {
-        var offsetPos = p5.Vector.lerp(previousPos, currentPos, this.iterator - currentIndex);
-        var heading = atan2(currentPos.y - previousPos.y, currentPos.x - previousPos.x) - HALF_PI;
-
-        push();
-        translate(offsetPos.x, offsetPos.y);
-        this.pendulum.update(heading);
-        if (showPendulum) {
-          this.pendulum.draw();
-        }
-        pop();
-
-        this.pendulumPath.push(this.pendulum.getTrail(offsetPos));
-      }
-    }
-
-    if (showPendulumPath) {
-      strokeWeight(1.6);
-      stroke(this.pendulumPathColor);
-      beginShape();
-      this.pendulumPath.forEach(function(pos) {
-        vertex(pos.x, pos.y);
-      });
-      endShape();
-    }
-  };
-
-  Shape.prototype.update = function() {
-    this.iterator += this.resolution;
-    this.iterator = constrain(this.iterator, 0, this.shapePath.length);
-  };
-}
-
-function Pendulum(size, hierarchy) {
-  this.hierarchy = hierarchy - 1;
-  this.pendulumArm;
-  this.size = size;
-  this.angle = random(TAU);
-  this.origin = createVector(0, 0);
-  this.end = createVector(0, 0);
-  this.gravity = gravity;
-  this.damping = damping;
-  this.angularAcceleration = 0;
-  this.angularVelocity = 0;
-
-  if (this.hierarchy > 0) {
-    this.pendulumArm = new Pendulum(this.size / 1.5, this.hierarchy);
-  }
-
-  Pendulum.prototype.update = function(heading) {
-    this.end.set(this.origin.x + this.size * sin(this.angle), this.origin.y + this.size * cos(this.angle));
-
-    this.angularAcceleration = (-this.gravity / this.size) * sin(this.angle + heading);
-    this.angle += this.angularVelocity;
-    this.angularVelocity += this.angularAcceleration;
-    this.angularVelocity *= this.damping;
-
-    if (this.pendulumArm) {
-      this.pendulumArm.update(heading);
-    }
-  };
-
-  Pendulum.prototype.getTrail = function(offset, end) {
-    if (this.pendulumArm) {
-      if (end) {
-        end.add(this.end);
-      } else {
-        end = this.end.copy();
-      }
-      return this.pendulumArm.getTrail(offset, end);
-    } else {
-      return this.end.copy().add(end).add(offset);
-    }
-  };
-
-  Pendulum.prototype.draw = function() {
-    stroke(0, 40);
-    beginShape();
-    vertex(this.origin.x, this.origin.y);
-    vertex(this.end.x, this.end.y);
-    endShape();
-
-    fill(0, 20);
-    ellipse(this.end.x, this.end.y, 2, 2);
-    noFill();
-
-    if (this.pendulumArm) {
-      push();
-      translate(this.end.x, this.end.y);
-      this.pendulumArm.draw();
-      pop();
-    }
-  };
-
-}
-
-function mousePressed() {
-  newShape = new Shape(color(random(360), 80, 60, 50));
-  newShape.addPos(mouseX, mouseY);
-}
-
-function mouseReleased() {
-  shapes.push(newShape);
-  newShape = undefined;
-}
-
-
-
-
-
-
 var _slicedToArray = function () {function sliceIterator(arr, i) {var _arr = [];var _n = true;var _d = false;var _e = undefined;try {for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {_arr.push(_s.value);if (i && _arr.length === i) break;}} catch (err) {_d = true;_e = err;} finally {try {if (!_n && _i["return"]) _i["return"]();} finally {if (_d) throw _e;}}return _arr;}return function (arr, i) {if (Array.isArray(arr)) {return arr;} else if (Symbol.iterator in Object(arr)) {return sliceIterator(arr, i);} else {throw new TypeError("Invalid attempt to destructure non-iterable instance");}};}();var SPAWN_RATE = [10, 30];
 var MAX_GROWTH_TIME = 10;
 var PAN_RANGE = [-0.8, 0.8];
@@ -903,6 +722,185 @@ StartAudioContext(Tone.context, canvas);
 
 
 
+
+
+
+
+'use strict';
+
+var shapes = [];
+
+var newShape;
+
+var joints = 12;
+var linelength = 64;
+var resolution = 0.06;
+var gravity = 0.094;
+var damping = 0.998;
+
+var showPath = true;
+var showPendulum = true;
+var showPendulumPath = true;
+var clearScreen = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+  noFill();
+  strokeWeight(1);
+}
+
+function draw() {
+  if (clearScreen) background(0, 0, 100);
+
+  shapes.forEach(function(shape) {
+    shape.draw();
+    shape.update();
+  });
+
+  if (newShape) {
+    newShape.addPos(mouseX, mouseY);
+    newShape.draw();
+    newShape.update();
+  }
+}
+
+function Shape(pendulumPathColor) {
+  this.shapePath = [];
+  this.pendulumPath = [];
+  this.pendulumPathColor = pendulumPathColor;
+  this.iterator = 0;
+  this.linelength = linelength;
+  this.resolution = resolution;
+  this.pendulum = new Pendulum(this.linelength, joints);
+
+  Shape.prototype.addPos = function(x, y) {
+    var newPos = createVector(x, y);
+    this.shapePath.push(newPos);
+  };
+
+  Shape.prototype.draw = function() {
+    strokeWeight(0.8);
+    stroke(0, 10);
+
+    if (showPath) {
+      beginShape();
+      this.shapePath.forEach(function(pos) {
+        vertex(pos.x, pos.y);
+      });
+      endShape();
+    }
+
+    if (this.iterator < this.shapePath.length) {
+      var currentIndex = floor(this.iterator);
+
+      var currentPos = this.shapePath[currentIndex];
+      var previousPos = this.shapePath[currentIndex - 1];
+      if (previousPos) {
+        var offsetPos = p5.Vector.lerp(previousPos, currentPos, this.iterator - currentIndex);
+        var heading = atan2(currentPos.y - previousPos.y, currentPos.x - previousPos.x) - HALF_PI;
+
+        push();
+        translate(offsetPos.x, offsetPos.y);
+        this.pendulum.update(heading);
+        if (showPendulum) {
+          this.pendulum.draw();
+        }
+        pop();
+
+        this.pendulumPath.push(this.pendulum.getTrail(offsetPos));
+      }
+    }
+
+    if (showPendulumPath) {
+      strokeWeight(1.6);
+      stroke(this.pendulumPathColor);
+      beginShape();
+      this.pendulumPath.forEach(function(pos) {
+        vertex(pos.x, pos.y);
+      });
+      endShape();
+    }
+  };
+
+  Shape.prototype.update = function() {
+    this.iterator += this.resolution;
+    this.iterator = constrain(this.iterator, 0, this.shapePath.length);
+  };
+}
+
+function Pendulum(size, hierarchy) {
+  this.hierarchy = hierarchy - 1;
+  this.pendulumArm;
+  this.size = size;
+  this.angle = random(TAU);
+  this.origin = createVector(0, 0);
+  this.end = createVector(0, 0);
+  this.gravity = gravity;
+  this.damping = damping;
+  this.angularAcceleration = 0;
+  this.angularVelocity = 0;
+
+  if (this.hierarchy > 0) {
+    this.pendulumArm = new Pendulum(this.size / 1.5, this.hierarchy);
+  }
+
+  Pendulum.prototype.update = function(heading) {
+    this.end.set(this.origin.x + this.size * sin(this.angle), this.origin.y + this.size * cos(this.angle));
+
+    this.angularAcceleration = (-this.gravity / this.size) * sin(this.angle + heading);
+    this.angle += this.angularVelocity;
+    this.angularVelocity += this.angularAcceleration;
+    this.angularVelocity *= this.damping;
+
+    if (this.pendulumArm) {
+      this.pendulumArm.update(heading);
+    }
+  };
+
+  Pendulum.prototype.getTrail = function(offset, end) {
+    if (this.pendulumArm) {
+      if (end) {
+        end.add(this.end);
+      } else {
+        end = this.end.copy();
+      }
+      return this.pendulumArm.getTrail(offset, end);
+    } else {
+      return this.end.copy().add(end).add(offset);
+    }
+  };
+
+  Pendulum.prototype.draw = function() {
+    stroke(0, 40);
+    beginShape();
+    vertex(this.origin.x, this.origin.y);
+    vertex(this.end.x, this.end.y);
+    endShape();
+
+    fill(0, 20);
+    ellipse(this.end.x, this.end.y, 2, 2);
+    noFill();
+
+    if (this.pendulumArm) {
+      push();
+      translate(this.end.x, this.end.y);
+      this.pendulumArm.draw();
+      pop();
+    }
+  };
+
+}
+
+function mousePressed() {
+  newShape = new Shape(color(random(360), 80, 60, 50));
+  newShape.addPos(mouseX, mouseY);
+}
+
+function mouseReleased() {
+  shapes.push(newShape);
+  newShape = undefined;
+}
 
 
 
