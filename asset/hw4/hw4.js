85da34535a0229840f9875b7bb293b0a09b4eaa1
TaewanGhim <wany@kaist.ac.kr>
2018-12-13 10:19:54 +0900

    Update hw4.js

--- asset/hw4/hw4.js
+++ asset/hw4/hw4.js
@@ -1,6 +1,5 @@
 var _slicedToArray = function () {function sliceIterator(arr, i) {var _arr = [];var _n = true;var _d = false;var _e = undefined;try {for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {_arr.push(_s.value);if (i && _arr.length === i) break;}} catch (err) {_d = true;_e = err;} finally {try {if (!_n && _i["return"]) _i["return"]();} finally {if (_d) throw _e;}}return _arr;}return function (arr, i) {if (Array.isArray(arr)) {return arr;} else if (Symbol.iterator in Object(arr)) {return sliceIterator(arr, i);} else {throw new TypeError("Invalid attempt to destructure non-iterable instance");}};}();var SPAWN_RATE = [10, 30];
-
-var MAX_GROWTH_TIME = 30;
+var MAX_GROWTH_TIME = 10;
 var PAN_RANGE = [-0.8, 0.8];
 
 var compressor = new Tone.Compressor().toMaster();
@@ -26,15 +25,18 @@ var INTERVALS = [1, 2, 3, 6];
 
 var MODES = [
 {
-  color: { hue: random(0,100), saturation: random(0,100)},
-  filterQ: 2000,
+  color: { hue: random(0,100), saturation: random(0,100) },
+   filterQ: 2000,
   gainRange: [0.5, 10],
   lengthRange: [10, 30],
   angleRange: [20, 40],
   maxLineLength: 10000,
   growthFactor: 20,
   lSystem: {
-    axiom: [{ symbol: "X", terminalAge: 0 }],
+    axiom: [
+    {
+      symbol: "X",
+      terminalAge: 0 }],
 
 
     productions: {
